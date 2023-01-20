c36e5296edb4ea4ba3df7eefd8dd3d55b2a7c3d7
Dean Yim <donghyeon0226@gmail.com>
2023-01-19 18:22:48 -0800

    added spotifyservice

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,17 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { HandtrackerComponent } from './handtracker/handtracker/handtracker.component';
+import { HandtrackerComponent } from './handtracker/handtracker.component';
 
 import { CarouselCardComponent } from './components/carousel-card/carousel-card.component';
 import { TrackListComponent } from './components/track-list/track-list.component';
-import { AboutComponent } from './about/about.component';
+import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
 import { ThermometerComponent } from './components/thermometer/thermometer.component';
 import { AlbumPageComponent } from './pages/album-page/album-page.component';
 import { ArtistPageComponent } from './pages/artist-page/artist-page.component';
 import { TrackPageComponent } from './pages/track-page/track-page.component';
-import { SpotifyServiceComponent } from './services/spotify.service/spotify.service.component';
 
 
 @NgModule({
@@ -33,8 +32,7 @@ import { SpotifyServiceComponent } from './services/spotify.service/spotify.serv
     AlbumPageComponent,
     ArtistPageComponent,
     TrackPageComponent,
-    SpotifyServiceComponent,
-
+   
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/services/spotify.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { ArtistData } from '../data/artist-data';
+import { AlbumData } from '../data/album-data';
+import { TrackData } from '../data/track-data';
+import { ResourceData } from '../data/resource-data';
+import { ProfileData } from '../data/profile-data';
+import { TrackFeature } from '../data/track-feature';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+}
